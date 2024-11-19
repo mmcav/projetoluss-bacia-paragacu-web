000b50df7433fe41225765c4fd660b74fb1a92e7
Marcelo <19808040+mmcav@users.noreply.github.com>
2024-11-18 21:49:03 -0300

    feat: substituidos os links de retorno ao topo por botões WIP: implementação de gráficos de barras em andamento

---
+++ public/botaoTopo.js
@@ -0,0 +1,17 @@
+const backToTopButton = document.querySelector('.back-to-top');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 100) {
+    backToTopButton.style.display = 'block';
+  } else {
+    backToTopButton.style.display = 'none';
+  }
+});
+
+backToTopButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
